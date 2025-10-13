885bedd45505d6147f852cc51110ef61f9a0a558
shahzod88888 <islomovdilbek01@gmail.com>
2025-10-13 21:38:27 +0500

    kurs

--- Sh-javascript lesson-5/main.js
+++ Sh-javascript lesson-5/main.js
@@ -81,15 +81,248 @@
 // 		console.log(i);
 // 	}
 // }
-// 9 - masla
 
-// let a = +prompt('Son kiritng: ');
-// let b = +prompt('Son kiritng: ');
-// let result = 0;
-// for (let i = a; i <= b; i++) {
-// 	result += i * i;
-// 	console.log(result);
+// 11 - masala
+
+// for (let a = 2; a <= 101; a+=2) {
+// 	console.log(a);
 // }
+
+// 12 - masala
+
+// for (let i = 2; i <= 105; i += 2) {
+// 	console.log(i);
+// }
+
+// 13 - masala
+
+// for (let i = 0; i <= 120; i += 2) {
+// 	console.log(i);
+// }
+
+// 14 - masala
+
+// for (let i = 120; i >= 100; i--) {
+// 	console.log(i);
+// }
+
+// 15 - masala
+
+// let n = +prompt('N sonni kiritng: ');
+// let m = +prompt('N sonni kiritng: ');
+// for (let i = n; i <= m; i++) {
+// 	console.log(i);
+// }
+
+// 16 - masala
+
+// let n = +prompt('N sonni kiritng: ');
+// let m = +prompt('N sonni kiritng: ');
+// for (let i = n; i <= m; i++) {
+// 	if (i % 2 != 0) {
+// 		console.log(i);
+// 	}
+// }
+
+// 17 - masala
+
+// let n = +prompt('N sonni kiritng: ');
+// let m = +prompt('N sonni kiritng: ');
+// for (let i = n; i >= m; i--) {
+// 	console.log(i);
+// }
+
+// ======================
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+
+// if (n > m) {
+// 	for (let i = n; i >= m; i--) {
+// 		console.log(i);
+// 	}
+// } else {
+// 	for (let i = n; i <= m; i++) {
+// 		console.log(i);
+// 	}
+// }
+
+// 18 - masala
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+// if (n > m) {
+// 	for (let i = n; i >= m; i--) {
+// 		if (i % 2 == 0) {
+// 			console.log(i);
+// 		} else {
+// 			for (let i = n; i <= m; i++) {
+// 				if (i % 2 == 0) {
+// 				}
+// 				console.log(i);
+// 			}
+// 		}
+// 	}
+// }
+
+// ===============================
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+
+// if (n > m) {
+// 	for (let i = n; i >= m; i--) {
+// 		if (i % 2 == 0) {
+// 			console.log(i);
+// 		}
+// 	}
+// } else {
+// 	for (let i = n; i <= m; i++) {
+// 		if (i % 2 == 0) {
+// 			console.log(i);
+// 		}
+// 	}
+// }
+
+// 19 - masala
+
+// let n = +prompt('N sonni kritng: ');
+
+// for (let i = n; i >= 0; i--) {
+// 	console.log(2 * i);
+// }
+
+// 20 - masala
+
+// let n = +prompt('N sonni kritng: ');
+
+// for (let i = n; i >= 0; i--) {
+// 	console.log(10 * i);
+// }
+
+// 21 - masala
+
+// for (let i = 1; i <= 10; i ++) {
+// 	console.log(i + i);
+// }
+
+// 22 - masala
+
+// for (let i = 20; i <= 30; i++) {
+// 	console.log(i + i);
+// }
+
+// 23 - masala
+
+// for (let i = -100; i <= 100; i++) {
+// 	console.log(i + i);
+// }
+
+// 24 - masala
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+
+// for (let i = n; i <= m; i++){
+//     console.log(i+i)
+// }
+
+// 25 - masala
+
+// for (let i = 1; i <= 10; i++) {
+// 	console.log(i * i);
+// }
+
+// 26 - masala
+
+// for (let i = 10; i <= 20; i++) {
+// 	console.log(i * i);
+// }
+
+// 27 - masala
+
+// let n = +prompt('N sonni kiritng: ');
+// let m = +prompt('N sonni kiritng: ');
+
+// for (let i = n; i <= m; i++) {
+// 	console.log(i * i);
+// }
+
+// 28 - masala
+
+// let n = +prompt('N sonni kiritng: ');
+// let m = +prompt('N sonni kiritng: ');
+// if (n > m) {
+// 	for (let i = n; i <= m; i += 2) {
+// 		console.log(i + i);
+// 	}
+// } else {
+// 	for (let i = n; i <= m; i += 2) {
+// 		console.log(i + i);
+// 	}
+// }
+
+// ==============================
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+
+// if (n > m) {
+// 	for (let i = n; i >= m; i -= 2) {
+// 		console.log(i);
+// 	}
+// } else {
+// 	for (let i = n; i <= m; i += 2) {
+// 		console.log(i);
+// 	}
+// }
+
+// 29 - masala
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+
+// for (let i = n; i <= m; i++) {
+// 	if (i % 2 == 1) {
+// 		console.log(i * i);
+// 	} else {
+// 		for (let i = n; i <= m; i += 2) {
+// 			console.log(i);
+// 		}
+// 	}
+// }
+
+// ==========================
+
+// let n = +prompt('N sonni kiriting: ');
+// let m = +prompt('M sonni kiriting: ');
+
+// for (let i = n; i <= m; i++) {
+// 	if (i % 2 == 1) {
+// 		console.log(i * i);
+// 	} else {
+// 		console.log(i);
+// 	}
+// }
+
+// 30 - masala
+
+// for (let n = 2; n <= 100; n++) {
+// 	let tub = true;
+
+// 	for (let i = 2; i < n; i++) {
+// 		if (n % i == 0) {
+// 			tub = false;
+// 			break;
+// 		}
+// 	}
+
+// 	if (tub) {
+// 		console.log(n);
+// 	}
+// }
+
+// 💡 Qudrat Abdurahimov Masalalar (1 - 15 masalalar)
 
 // 8 - masla
 
@@ -101,14 +334,24 @@
 // 	console.log(result);
 // }
 
+// 9 - masla
+
+// let a = +prompt('Son kiritng: ');
+// let b = +prompt('Son kiritng: ');
+// let result = 0;
+// for (let i = a; i <= b; i++) {
+// 	result += i * i;
+// 	console.log(result);
+// }
+
 // 12 - masala
 
-let n = 10;
-let natija = 1;
+// let n = 10;
+// let natija = 1;
 
-for (let i = 1.1; i <= n; i += 0.1) {
-    natija *= i;
-    console.log(i)
-}
+// for (let i = 1.1; i <= n; i += 0.1) {
+// 	natija *= i;
+// 	console.log(i);
+// }
 
-console.log(natija);
+// console.log(natija);
