6d72c0d83f0a10bf07037c28cf97111c9c3c83db
shahzod88888 <islomovdilbek01@gmail.com>
2025-11-07 16:39:21 +0500

    kurs

---
+++ Sh-javascript lesson-10/main.js
@@ -0,0 +1,49 @@
+// Array Theme
+
+// Number Array
+
+// let numberArray = [3, 8, 9, 5, 2, 1, 3, 6];
+
+// Srting Array
+
+// let stringArray = ['hello', 'js', 'html'];
+
+// Boolean Array
+
+// let booleanArray = [true, false, false];
+
+// Object Array
+
+// let objectArray = [{ user1 }, { user2 }, { user3 }];
+
+// Mix Array
+
+// let mixArray = ['salom', 12, true, [], {}];
+
+// Nested Array
+
+// let nestedArray = [[], [], []];
+
+// ==================================
+
+let NumberArray = [9, 3, 6, 45, 1, 7, 9, 0];
+
+// array length
+// console.log(NumberArray.length);
+
+// oxiridan element qo'shish
+
+NumberArray.push(23);
+
+// boshidan element qo'shish
+
+NumberArray.unshift(10);
+
+// oxirirdan bitta element o'chiradi
+
+NumberArray.pop();
+
+// boshidan bitta element o'chiradi
+
+NumberArray.shift();
+console.log(NumberArray);
