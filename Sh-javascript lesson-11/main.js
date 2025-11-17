0bf105953dd9b6224ad79417642070c74d5a4de4
Shakhzod16 <qosimov0805@gmail.com>
2025-11-17 15:25:08 +0500

    lesson

--- Sh-javascript lesson-11/main.js
+++ Sh-javascript lesson-11/main.js
@@ -126,16 +126,91 @@
 // }
 
 // Homework
+// const btn1 = document.getElementById('btn1');
+// const btn2 = document.getElementById('btn2');
+// const circle1 = document.getElementById('circle1');
+// const circle2 = document.getElementById('circle2');
+// const circle3 = document.getElementById('circle3');
 
-// function draw() {
-// 	if (random === 1) card1.style.backgroundColor = 'blue';
-// 	else if (random === 2) card2.style.backgroundColor = 'blue';
-// 	else if (random === 3) card3.style.backgroundColor = 'blue';
-// 	else if (random === 4) card4.style.backgroundColor = 'blue';
-// 	else if (random === 5) card5.style.backgroundColor = 'blue';
-// 	else if (random === 6) card6.style.backgroundColor = 'blue';
-// 	else if (random === 7) card7.style.backgroundColor = 'blue';
-// 	else if (random === 8) card8.style.backgroundColor = 'blue';
-// 	else if (random === 9) card9.style.backgroundColor = 'blue';
-// 	else if (random === 10) card10.style.backgroundColor = 'blue';
+// let id;
+// let stage = 'red';
+// let count = 10;
+
+// btn1.addEventListener('click', startBtn);
+// btn2.addEventListener('click', stopBtn);
+
+// function startBtn() {
+// 	clearInterval(id);
+
+// 	stage = 'red';
+// 	count = 10;
+
+// 	id = setInterval(() => {
+// 		if (stage === 'red') {
+// 			circle1.style.background = 'red';
+// 			circle2.style.background = 'white';
+// 			circle3.style.background = 'white';
+// 			circle1.innerText = count;
+// 		}
+
+// 		if (stage === 'yellow') {
+// 			circle1.style.background = 'white';
+// 			circle2.style.background = 'yellow';
+// 			circle3.style.background = 'white';
+// 			circle2.innerText = count;
+// 		}
+
+// 		if (stage === 'green') {
+// 			circle1.style.background = 'white';
+// 			circle2.style.background = 'white';
+// 			circle3.style.background = 'green';
+// 			circle3.innerText = count;
+// 		}
+
+// 		count--;
+
+// 		if (count < 0) {
+// 			if (stage === 'red') {
+// 				stage = 'yellow';
+// 				count = 5;
+// 			} else if (stage === 'yellow') {
+// 				stage = 'green';
+// 				count = 10;
+// 			} else if (stage === 'green') {
+// 				stage = 'red';
+// 				count = 10;
+// 			}
+// 		}
+// 	}, 1000);
 // }
+
+// function stopBtn() {
+// 	clearInterval(id);
+
+// 	circle1.style.background = 'white';
+// 	circle2.style.background = 'white';
+// 	circle3.style.background = 'white';
+
+// 	circle1.innerText = '';
+// 	circle2.innerText = '';
+// 	circle3.innerText = '';
+// }
+
+// topshiriq
+
+const btn = document.getElementById('btn');
+const box = document.getElementById('changeColor');
+
+btn.addEventListener('click', start);
+
+let interval;
+
+function start() {
+	interval = setInterval(() => {
+		if (box.style.backgroundColor === 'red') {
+			box.style.backgroundColor = 'white';
+		} else {
+			box.style.backgroundColor = 'red';
+		}
+	}, 100);
+}
---
+++ Sh-javascript lesson-12/main.js
@@ -0,0 +1,188 @@
+// const text = document.getElementById('text');
+// const btn = document.getElementById('btn');
+// const input = document.getElementById('input');
+
+// let cars = ['cobalt', 'lacetti', 'matiz', 'nexia'];
+
+// btn.addEventListener('click', save);
+
+// function save() {
+// 	let inputValue = input.value;
+// 	if (cars.includes(inputValue)) {
+// 		alert('bunday avtomobile mavjud');
+// 	} else {
+// 		cars.push(inputValue);
+// 	}
+// 	draw();
+// }
+
+// function draw() {
+// 	text.innerText = cars;
+// }
+// draw();
+
+// Topshiriq
+
+// const input1 = document.getElementById('input1');
+// const input2 = document.getElementById('input2');
+// const plus = document.getElementById('plus');
+// const minus = document.getElementById('minus');
+// const kop = document.getElementById('kop');
+// const bol = document.getElementById('bol');
+// const natija = document.getElementById('natija');
+
+// plus.addEventListener('click', plusNum);
+// minus.addEventListener('click', minusNum);
+// kop.addEventListener('click', kopNum);
+// bol.addEventListener('click', bolNum);
+
+// function plusNum() {
+// 	let a = +input1.value;
+// 	let b = +input2.value;
+// 	natija.innerText = a + b;
+// }
+
+// function minusNum() {
+// 	let a = +input1.value;
+// 	let b = +input2.value;
+// 	natija.innerText = a - b;
+// }
+
+// function kopNum() {
+// 	let a = +input1.value;
+// 	let b = +input2.value;
+// 	natija.innerText = a * b;
+// }
+
+// function bolNum() {
+// 	let a = +input1.value;
+// 	let b = +input2.value;
+// 	natija.innerText = a / b;
+// }
+
+// ======================  Homework  ======================
+// <!-- 1 - topshiriq -->
+
+const inp = document.getElementById('inp');
+const btn1 = document.getElementById('btn1');
+const result = document.getElementById('result');
+const yigindi = document.getElementById('yigindi');
+const kopaytma = document.getElementById('kopaytma');
+const totalNum = document.getElementById('totalNum');
+
+btn1.addEventListener('click', saveNum);
+yigindi.addEventListener('click', plusNum);
+kopaytma.addEventListener('click', kopaytmaNum);
+
+let arr = [];
+
+function saveNum() {
+	let a = parseInt(inp.value);
+	if (arr.push(a)) {
+		inp.value = '';
+	}
+
+	result.innerText = arr;
+}
+
+function plusNum() {
+	let count = 0;
+	for (let i = 0; i < arr.length; i++) {
+		count += arr[i];
+	}
+	totalNum.textContent = count;
+}
+
+function kopaytmaNum() {
+	let sum = 1;
+	for (let i = 0; i < arr.length; i++) {
+		sum *= arr[i];
+	}
+	totalNum.textContent = sum;
+}
+
+// <!-- 2 - topshiriq -->
+
+// const inp1 = document.getElementById('inp1');
+// const inp2 = document.getElementById('inp2');
+// const inp3 = document.getElementById('inp3');
+// const pushsh = document.getElementById('push');
+// const unshiftt = document.getElementById('unshift');
+// const splicee = document.getElementById('splice');
+// const popp = document.getElementById('pop');
+// const shiftt = document.getElementById('shift');
+// const total = document.getElementById('total');
+
+// pushsh.addEventListener('click', pushWord);
+// unshiftt.addEventListener('click', unshiftWord);
+// splicee.addEventListener('click', spliceWord);
+// popp.addEventListener('click', popWord);
+// shiftt.addEventListener('click', shiftWord);
+
+// function pushWord() {
+// 	let text = inp1.value;
+
+// 	text = ',' + text;
+// 	total.textContent += text;
+// 	inp1.value = '';
+// }
+// function unshiftWord() {
+// 	let text = inp2.value;
+// 	total.textContent = text + ',' + total.textContent;
+// }
+
+// function spliceWord() {
+// 	let massiv = total.textContent.split(',');
+// 	let a = Number(inp3.value);
+// 	massiv.splice(a, 1);
+// 	total.textContent = massiv.join(',');
+// }
+
+// function popWord() {
+// 	let massiv = total.textContent.split(',');
+// 	massiv.pop();
+// 	total.textContent = massiv.join(',');
+// }
+// function shiftWord() {
+// 	let massiv = total.textContent.split(',');
+// 	massiv.shift();
+// 	total.textContent = massiv.join(',');
+// }
+
+// <!-- 3 - topshiriq -->
+
+// const inp1 = document.getElementById('inp1');
+// const inp2 = document.getElementById('inp2');
+// const btn = document.getElementById('btn');
+// const text = document.getElementById('text');
+
+// btn.addEventListener('click', addWord);
+
+// function addWord() {
+// 	let a = inp1.value;
+// 	let b = parseInt(inp2.value);
+// 	let text1 = a.repeat(b);
+// 	text1 = ',' + text1;
+// 	text.textContent = text.textContent + text1;
+// }
+
+// <!-- 4 - topshiriq -->
+
+// const inp = document.querySelector('input');
+// const btn = document.getElementById('btn');
+// const btn2 = document.getElementById('btn2');
+// const res = document.querySelector('.res');
+// const total1 = document.getElementById('total1');
+
+// let arr = [];
+
+// btn.addEventListener('click', () => {
+// 	arr.push(+inp.value);
+// 	res.textContent = arr;
+// 	inp.value = '';
+// });
+
+// btn2.addEventListener('click', () => {
+// 	let orta = arr.reduce((a, b) => a + b, 0) / arr.length;
+// 	total1.textContent = orta;
+// });
