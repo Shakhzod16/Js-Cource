f7e54e9b8146314523dd9888936a2e4cbb68ac2f
Shakhzod16 <qosimov0805@gmail.com>
2025-11-22 09:45:06 +0500

    lesson

--- Sh-javascript lesson-13/main.js
+++ Sh-javascript lesson-13/main.js
@@ -36,107 +36,137 @@
 
 // =============================
 
-// const text = document.getElementById('text');
-// const nameInput = document.getElementById('name_inp');
-// const ageInput = document.getElementById('age_inp');
-// const emailInput = document.getElementById('email_inp');
-// const btn = document.getElementById('btn');
-
-// let users = [
-// 	{ name: 'Nozim', age: 12, email: 'nozim@mail.com' },
-// 	{ name: 'Alisher', age: 27, email: 'alisher@mail.com' },
-// 	{ name: 'Qurbon', age: 63, email: 'qurbon@mail.com' },
-// 	{ name: 'Bobomurod', age: 43, email: 'bobomurod@mail.com' },
-// ];
-
-// btn.addEventListener('click', saveTable);
-
-// function saveTable() {
-// 	let userObj = {
-// 		name: nameInput.value,
-// 		age: +ageInput.value,
-// 		email: emailInput.value,
-// 	};
-// 	users.push(userObj);
-// 	clearForm();
-// 	draw();
-// }
-
-// function draw() {
-// 	let result = '';
-// 	for (let i = 0; i < users.length; i++) {
-// 		result += `<tr>
-//    <td>${users[i].name}</td>
-//    <td>${users[i].age}</td>
-//    <td>${users[i].email}</td>
-//     </tr>`;
-// 	}
-// 	text.innerHTML = result;
-// }
-// draw();
-
-// function clearForm() {
-// 	nameInput.value = '';
-// 	ageInput.value = '';
-// 	emailInput.value = '';
-// }
-
-// Homework
-
+const nameInput = document.getElementById('name_inp');
+const ageInput = document.getElementById('age_inp');
+const emailInput = document.getElementById('email_inp');
 const saveBtn = document.getElementById('save_btn');
-const text = document.getElementById('text');
-const nameInp = document.getElementById('name_inp');
-const taskArea = document.getElementById('task');
+const tbody = document.getElementById('tbody');
 
 saveBtn.addEventListener('click', save);
-let tasks = [];
+let users = [];
 let soqchi = '';
 
 function save() {
-	let taskObj = {
-		name: nameInp.value,
-		text: text.value,
+	let userObj = {
+		name: nameInput.value,
+		age: +ageInput.value,
+		email: emailInput.value,
 	};
-
 	if (soqchi === '') {
-		tasks.push(taskObj);
+		users.push(userObj);
 	} else {
-		tasks[soqchi] = taskObj;
+		users[soqchi] = userObj;
 		soqchi = '';
 	}
-
+	changeBtnText();
 	draw();
 	clearForm();
 }
 
-function deleteTask(index) {
-	tasks.splice(index, 1);
+function deleteUser(index) {
+	users.splice(index, 1);
 	draw();
 }
-function updateTask(index) {
-	let editingTask = tasks[index];
-	nameInp.value = editingTask.name;
-	text.value = editingTask.text;
+function updateUser(index) {
+	let editUser = users[index];
+	nameInput.value = editUser.name;
+	ageInput.value = editUser.age;
+	emailInput.value = editUser.email;
 	soqchi = index;
+	changeBtnText();
 }
 
 function draw() {
-	let res = '';
-	for (let i = 0; i < tasks.length; i++) {
-		res += `	<div class='card shadow-sm w-25'>
-				<div class='card-body'>
-					<h1>${tasks[i].name}</h1>
-					<p>${tasks[i].text}</p>
-				</div>
-				<div class= "p-2 btn-group" >
-       <button onclick= 'deleteTask(${i})' class= 'btn btn-danger'>x</button>
-       <button onclick= 'updateTask(${i})' class= 'btn btn-warning'>edit</button>
-       </div>
-			</div>`;
+	let result = '';
+	for (let i = 0; i < users.length; i++) {
+		result += `<tr>
+		<td>${i + 1}</td>
+		<td>${users[i].name}</td>
+		<td>${users[i].age}</td>
+		<td>${users[i].email}</td>
+    <td>
+    <button onclick= "deleteUser(${i})" class="btn btn-danger">🗑️</button>
+    <button onclick="updateUser(${i})" class="btn btn-warning" >✏️</button>
+    </td>
+		</tr>`;
 	}
-	taskArea.innerHTML = res;
+	tbody.innerHTML = result;
 }
+
 function clearForm() {
-	nameInp.value = '';
-	text.value = '';
+	nameInput.value = '';
+	ageInput.value = '';
+	emailInput.value = '';
 }
+
+function changeBtnText() {
+	if (soqchi === '') {
+		saveBtn.innerText = 'Save';
+		saveBtn.classList.remove('btn-primary');
+		saveBtn.classList.add('btn-dark');
+	} else {
+		saveBtn.innerText = 'Update';
+		saveBtn.classList.remove('btn-dark');
+		saveBtn.classList.add('btn-primary');
+	}
+}
+
+// Homework
+
+// const saveBtn = document.getElementById('save_btn');
+// const text = document.getElementById('text');
+// const nameInp = document.getElementById('name_inp');
+// const taskArea = document.getElementById('task');
+
+// saveBtn.addEventListener('click', save);
+// let tasks = [];
+// let soqchi = '';
+
+// function save() {
+// 	let taskObj = {
+// 		name: nameInp.value,
+// 		text: text.value,
+// 	};
+
+// 	if (soqchi === '') {
+// 		tasks.push(taskObj);
+// 	} else {
+// 		tasks[soqchi] = taskObj;
+// 		soqchi = '';
+// 	}
+
+// 	draw();
+// 	clearForm();
+// }
+
+// function deleteTask(index) {
+// 	tasks.splice(index, 1);
+// 	draw();
+// }
+// function updateTask(index) {
+// 	let editingTask = tasks[index];
+// 	nameInp.value = editingTask.name;
+// 	text.value = editingTask.text;
+// 	soqchi = index;
+// }
+
+// function draw() {
+// 	let res = '';
+// 	for (let i = 0; i < tasks.length; i++) {
+// 		res += `	<div class='card shadow-sm w-25'>
+// 				<div class='card-body'>
+// 					<h1>${tasks[i].name}</h1>
+// 					<p>${tasks[i].text}</p>
+// 				</div>
+// 				<div class= "p-2 btn-group" >
+//        <button onclick= 'deleteTask(${i})' class= 'btn btn-danger'>x</button>
+//        <button onclick= 'updateTask(${i})' class= 'btn btn-warning'>edit</button>
+//        </div>
+// 			</div>`;
+// 	}
+// 	taskArea.innerHTML = res;
+// }
+// function clearForm() {
+// 	nameInp.value = '';
+// 	text.value = '';
+// }
