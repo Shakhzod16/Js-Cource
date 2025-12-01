1c47cf69c94b13837cf600830e8489124f68b952
Shakhzod16 <qosimov0805@gmail.com>
2025-12-01 08:53:42 +0500

    lesson

--- Sh-javascript lesson-16/main.js
+++ Sh-javascript lesson-16/main.js
@@ -108,3 +108,117 @@
 // }
 
 // <!-- Homework - Task - 3 -->
+
+// const box = document.getElementById('box');
+
+// function test(e) {
+// 	let color = e.target.value;
+// 	draw(color);
+// }
+
+// function draw(son) {
+// 	if (son === 'BLUE') {
+// 		box.style.backgroundColor = 'blue';
+// 	} else if (son === 'RED') {
+// 		box.style.backgroundColor = 'red';
+// 	} else if (son === 'BLACK') {
+// 		box.style.backgroundColor = 'black';
+// 	} else if (son === 'YELLOW') {
+// 		box.style.backgroundColor = 'yellow';
+// 	} else if (son === 'GREEN') {
+// 		box.style.backgroundColor = 'green';
+// 	}
+// }
+
+// <!-- Homework - Task - 4 -->
+
+const nameInp = document.getElementById('name_inp');
+const priceInp = document.getElementById('price_inp');
+const numInp = document.getElementById('num_inp');
+const saveBtn = document.getElementById('save_btn');
+const productsDiv = document.getElementById('products');
+
+let users = [];
+let editIndex = -1;
+
+saveBtn.addEventListener('click', function () {
+	if (editIndex === -1) {
+		createItem();
+	} else {
+		updateItem();
+	}
+});
+
+function createItem() {
+	if (!nameInp.value.trim()) return alert('Kitob nomini kiriting');
+	let userObj = {
+		name: nameInp.value.trim(),
+		price: priceInp.value.trim(),
+		number: numInp.value.trim(),
+	};
+	users.push(userObj);
+	clearInputs();
+	drawProducts();
+}
+
+function updateItem() {
+	if (editIndex < 0 || editIndex >= users.length) return;
+
+	users[editIndex] = {
+		name: nameInp.value.trim(),
+		price: priceInp.value.trim(),
+		number: numInp.value.trim(),
+	};
+
+	editIndex = -1;
+	saveBtn.innerText = 'Save';
+	clearInputs();
+	drawProducts();
+}
+
+function clearInputs() {
+	nameInp.value = '';
+	priceInp.value = '';
+	numInp.value = '';
+}
+
+function drawProducts() {
+	let html = '';
+	for (let i = 0; i < users.length; i++) {
+		html += `
+      <div class="card p-3 mt-2">
+        <h5>${users[i].name}</h5>
+        <p>Narxi: ${users[i].price}</p>
+        <p>Soni: ${users[i].number}</p>
+        <div class="btn-group">
+          <button class="btn btn-warning btn-sm" onclick="editItem(${i})">Edit</button>
+          <button class="btn btn-danger btn-sm" onclick="deleteItem(${i})">Delete</button>
+        </div>
+      </div>
+    `;
+	}
+	productsDiv.innerHTML = html;
+}
+
+function deleteItem(i) {
+	users.splice(i, 1);
+
+	if (editIndex === i) {
+		editIndex = -1;
+		saveBtn.innerText = 'Save';
+		clearInputs();
+	} else if (editIndex > i) {
+		editIndex--;
+	}
+	drawProducts();
+}
+
+function editItem(i) {
+	editIndex = i;
+	nameInp.value = users[i].name;
+	priceInp.value = users[i].price;
+	numInp.value = users[i].number;
+	saveBtn.innerText = 'Update';
+
+	nameInp.focus();
+}
