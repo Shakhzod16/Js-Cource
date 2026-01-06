d62a99f809f5542ff8aba9e998f3baec47df2fa9
Shakhzod16 <qosimov0805@gmail.com>
2026-01-06 16:39:32 +0500

    lesson

---
+++ Sh-javascript lesson-23/main.js
@@ -0,0 +1,33 @@
+"use strict";
+// let a: number = 5;
+// a = 34;
+// console.log(a);
+// Masalalar
+// 1 - masala
+// let num1: number = 3;
+// let num2: number = 5;
+// let num3: number = num1 * num2;
+// console.log(num3);
+// 2 - masala
+// let str1: string = 'Hello';
+// let str2: string = 'World';
+// let str3: string = str1 + str2;
+// console.log(str3);
+// 3 - masala
+// let num1: number = 70;
+// let num2: number = 15;
+// let num3: number = 30;
+// let num4: number = num1 - num2 - num3;
+// console.log(num4);
+// 4 - masala
+// let str1: string = 'Hello';
+// let str2: string = 'World';
+// let str3: string = str1.charAt(0) + str2.slice(-1);
+// console.log(str3);
+// 4 - masala
+// let num1: number = 20;
+// let num2: number = 10;
+// let num3: number = 30;
+// let num4: number = num1 + num2 + num3;
+// console.log(num4);
+//# sourceMappingURL=main.js.map
